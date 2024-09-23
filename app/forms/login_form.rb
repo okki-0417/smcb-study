f10f7a4b32d0ffe7456d8c77efc379128587d6fd
okki-0417 <ochan0901@gmail.com>
2024-09-23 16:59:26 +0900

    カスタムバリデーションの指定方法をちょっと変えた

--- app/forms/login_form.rb
+++ app/forms/login_form.rb
@@ -5,6 +5,6 @@ class LoginForm < BaseForm
   attr_accessor :password
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, "email": true
-  validates :password, "password_weak": true
+  validates :email, email: true
+  validates :password, password_weak: true
 end
