c36e11c30031d9145af6e0ceca8579ba82b8f963
okki-0417 <ochan0901@gmail.com>
2024-09-29 14:42:18 +0900

    userが存在しないバリデーターを参照していた

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { minimum: 1, maximum: 30 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true, password_weak: true
+  validates :password, presence: true, length: { minimum: 6, maximum: 256 }
 
   has_secure_password
 
